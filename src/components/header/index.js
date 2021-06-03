da274d2b775e2c0ced45367bc712fa56fb6f0985
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-03 12:34:50 -0300

    elementos do header alinhados

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,10 +7,10 @@ export default function Header(){
 
                 <div className="ContainerLeft">
                     <div className="element-left menu">
-                        <img src="./img/menu.png" style={{width: 18, height: 16}} />
+                        <img src="./img/menu.png" style={{width: 18, height: 16}} alt="menu" />
                     </div>
                     <div className="element-left logo">
-                        <img src="./img/youtube-logo.png" style={{width: 91, height: 20}}/>
+                        <img src="./img/youtube-logo.png" style={{width: 91, height: 20}} alt="logo"/>
                     </div>
                 </div>
 
@@ -19,22 +19,22 @@ export default function Header(){
                         <input type="text" placeholder="Pesquisar" className="search"/>
                     </div>
                     <div className="searchback">
-                        <img src="./img/search.png" className="search-icon"/>
+                        <img src="./img/search.png" className="search-icon" alt="lupinha"/>
                     </div>
                     <div className="element-center">
-                        <img src="./img/microphone.png" />
+                        <img src="./img/microphone.png"  alt="microfone"/>
                     </div>
                 </div>
 
                 <div className="ContainerRight">
                     <div className="element-right camera">
-                        <img src="./img/video-camera.png" />
+                        <img src="./img/video-camera.png" alt="camera"/>
                     </div>
                     <div className="element-right grid">
-                        <img src="./img/grid.png" />
+                        <img src="./img/grid.png" alt="grid"/>
                     </div>
                     <div className="element-right bell">
-                        <img src="./img/bell.png" />
+                        <img src="./img/bell.png" alt="bell"/>
                     </div>
                     <div className="element-right user"></div>
                 </div>
