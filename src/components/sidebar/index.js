fc6924e87e9153daa6db67465ea25b60f5fad8af
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-07 15:32:40 -0300

    fix errors

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -5,20 +5,20 @@ export default function Sidebar(){
     return(
         <section>
             <div className="sidebar inicio">
-                <img src="./img/home.png"/>
+                <img src="./img/home.png" className="home"/>
                 <p className="pardif"> Início </p>
             </div>
             <div className="sidebar">
-                <img src="./img/explore.png"/>
+                <img src="./img/explore.png" className="icon"/>
                 <p className="explor"> Explorar </p>
             </div>
             <div className="sidebar">
-                <img src="./img/subscriptions.png"/>
+                <img src="./img/subscriptions.png" className="icon"/>
                 <p className="subs"> Inscrições </p>
             </div>
             <div className="sidebar">
-                <img src="./img/library.png"/>
-                <p className="subs"> Biblioteca </p>
+                <img src="./img/library.png" className="icon"/>
+                <p className="libs"> Biblioteca </p>
             </div>
         </section>
     );
