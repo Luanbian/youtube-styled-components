11c7a19848c7610b9e8f5074bc328faead4451a2
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-07 13:37:56 -0300

    iniciando sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/index";
 import Slideshow from './components/categories/index';
+import Sidebar from './components/sidebar/index';
 
 export default function App(){
     return(
         <div>
             <Header />
             <Slideshow />
+            <Sidebar />
             <div className="app">
                 <h1>Teste</h1>
             </div>
