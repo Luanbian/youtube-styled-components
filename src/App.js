c10807c7984325264ed3faf187be6df842206927
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-08 16:36:08 -0300

    inserindo menu lateral

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from "./components/header/index";
 import Slideshow from './components/categories/index';
 import Sidebar from './components/sidebar/index';
-
+import Menubar from './components/sidemenu/menubar';
 export default function App(){
     return(
         <div>
             <Header />
             <Slideshow />
             <Sidebar />
+            <Menubar />
             <div className="app">
             </div>
         </div>
