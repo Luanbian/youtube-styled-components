4acf569dc197223f90de08585b99c1649895bf36
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-17 13:05:48 -0300

    iniciando na main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from "./components/header/index";
 import Slideshow from './components/categories/index';
 import Sidebar from './components/sidebar/index';
 import Menubar from './components/sidemenu/menubar';
+import Main from './components/main/index';
+
 export default function App(){
     return(
         <div>
@@ -10,6 +12,7 @@ export default function App(){
             <Slideshow />
             <Sidebar />
             <div className="app">
+                <Main />
             </div>
         </div>
     );
