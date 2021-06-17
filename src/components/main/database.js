0693792756aa70626687d42b339669bfe53e8c3e
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-17 15:45:57 -0300

    iniciando na criação dos cards na main

---
+++ src/components/main/database.js
@@ -0,0 +1,20 @@
+const database = [
+    {
+      title: 'Trombinoscope',
+      image: "oi",
+      time: '59:59',
+      imageicon: 'icon',
+      name: 'luan teste',
+      vistime: '1000 visualizações . há 8 meses'
+    },
+    {
+      title: 'CRUD System',
+      image: 'url(https://i.ibb.co/g4mT3K5/html-Css-Js.jpg)',
+      time: '59:59',
+      imageicon: 'icon',
+      name: 'luan teste',
+      vistime: '1000 visualizações . há 8 meses'
+    }
+]
+  
+export default database;
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './main.css';
+import database from './database';
 
-export default function Main(){
+export default function Main() {
     return(
         <div>
-            Teste
+            {database.map(database => (
+                <div>
+                    <div className="card">
+                        <div> 
+                            {database.image}{/* Imagem do video */}
+                        </div>
+                        <div>
+                            {/* Tempo do video */}
+                        </div>
+                        <div>
+                            {/* Titulo do video */}
+                        </div>
+                        <div>
+                            {/* icone do criador do video */}
+                        </div>
+                        <div>
+                            {/* Nome do canal */}
+                        </div>
+                        <div>
+                            {/* Num. de visualizações . Tempo que foi postado */}
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
