bc23c39952c1bdaf728f27c4f17d8ec1f8967bcb
Luan de Almeida <luanbianchine1@gmail.com>
2021-06-17 19:14:46 -0300

    ajustes no card da main

--- src/components/main/database.js
+++ src/components/main/database.js
@@ -1,11 +1,11 @@
 const database = [
   {
-    title: 'Video Title',
+    title: 'Iniciando com Node.JS',
     image: <img className="card-image" src="https://greepx.com/wp-content/uploads/2018/01/1515862799_579_lo-fi.jpg"/>,
-    time: '59:59',
+    time: '23:45',
     imageicon: <img  className="card-icon" src="./img/rocket.jpg" />,
-    name: 'luan teste',
-    vistime: '1000 visualizações . há 8 meses'
+    name: 'Rocketseat',
+    vistime: '6,1 mil visualizações . há 8 meses'
   },
 ]
   
